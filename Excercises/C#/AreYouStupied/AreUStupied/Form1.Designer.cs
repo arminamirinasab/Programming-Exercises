2741b09ee94de9b420b1d48ff5989890ba2c7a47
arminamirinasab <arminamirinasab@gmail.com>
2022-12-14 17:00:33 +0330

    Add AreYouStupied Excercise C#

---
+++ Excercises/C#/AreYouStupied/AreUStupied/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace AreUStupied
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnYes = new System.Windows.Forms.Button();
+            this.btnNo = new System.Windows.Forms.Button();
+            this.btnX = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Lucida Sans Unicode", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(32, 20);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lblResult.Size = new System.Drawing.Size(209, 23);
+            this.lblResult.TabIndex = 0;
+            this.lblResult.Text = "Are You Stupied ^0^";
+            this.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblResult.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnYes
+            // 
+            this.btnYes.FlatAppearance.BorderSize = 0;
+            this.btnYes.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnYes.Location = new System.Drawing.Point(12, 89);
+            this.btnYes.Name = "btnYes";
+            this.btnYes.Size = new System.Drawing.Size(216, 103);
+            this.btnYes.TabIndex = 1;
+            this.btnYes.Text = "Yes";
+            this.btnYes.UseVisualStyleBackColor = true;
+            this.btnYes.Click += new System.EventHandler(this.btnYes_Click);
+            // 
+            // btnNo
+            // 
+            this.btnNo.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnNo.Location = new System.Drawing.Point(83, 60);
+            this.btnNo.Name = "btnNo";
+            this.btnNo.Size = new System.Drawing.Size(75, 23);
+            this.btnNo.TabIndex = 2;
+            this.btnNo.Text = "No";
+            this.btnNo.UseVisualStyleBackColor = true;
+            this.btnNo.Click += new System.EventHandler(this.btnNo_Click);
+            // 
+            // btnX
+            // 
+            this.btnX.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnX.Location = new System.Drawing.Point(196, 23);
+            this.btnX.Name = "btnX";
+            this.btnX.Size = new System.Drawing.Size(24, 23);
+            this.btnX.TabIndex = 3;
+            this.btnX.Text = "X";
+            this.btnX.UseVisualStyleBackColor = true;
+            this.btnX.Visible = false;
+            this.btnX.Click += new System.EventHandler(this.btnX_Click);
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(232, 203);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnX);
+            this.Controls.Add(this.btnNo);
+            this.Controls.Add(this.btnYes);
+            this.Controls.Add(this.lblResult);
+            this.Font = new System.Drawing.Font("Lucida Sans Unicode", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "mainForm";
+            this.Opacity = 0.95D;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnYes;
+        private System.Windows.Forms.Button btnNo;
+        private System.Windows.Forms.Button btnX;
+    }
+}
+
---
+++ Excercises/C#/AreYouStupied/AreUStupied/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AreUStupied
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        // 
+        Random myRandom = new Random();
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // Yes Button Event
+        private void btnYes_Click(object sender, EventArgs e)
+        {
+            lblResult.Text = "I knew It O_O";
+            btnYes.Visible = false;
+            btnNo.Visible = false;
+            this.Height = 100;
+            btnX.Visible = true;
+        }
+
+        // No Button Event
+        private void btnNo_Click(object sender, EventArgs e)
+        {
+
+            btnNo.Location = new Point(myRandom.Next(0, 200), myRandom.Next(0, 200));
+        }
+
+        // Close Event
+        private void btnX_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
