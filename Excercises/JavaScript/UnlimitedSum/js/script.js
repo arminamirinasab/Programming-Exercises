9593fced416d35bec23e633db4a1c1f46f19ebec
arminamirinasab <arminamirinasab@gmail.com>
2022-12-16 15:12:02 +0330

    Refactor Last Exercise

--- Excercises/JavaScript/UnlimitedSum/js/script.js
+++ Excercises/JavaScript/UnlimitedSum/js/script.js
@@ -1,9 +1,22 @@
+// With Function Declaration
+
 function sum() {
   let sum = 0;
-  for (let i = 0; i < arguments.length; i++) {
-    sum += arguments[i];
+  let Args = Array.from(arguments);
+  for (let i = 0; i < Args.length; i++) {
+    sum += Args[i];
   }
   return sum;
 }
 
-console.log(sum(10 , 20 , 5 , 7 , 47 , 89 , 36 , 14 , 15 , 71 , 23 , 12 , 39));
+// With Arrow Function
+/*
+let sum = (...args) => {
+    let sum = 0;
+    for (let i = 0; i < args.length; i++) {
+      sum += args[i];
+    }
+    return sum;
+}
+*/
+console.log(sum(10, 20, 5, 7, 47, 89, 36, 14, 15, 71, 23, 12, 39));
