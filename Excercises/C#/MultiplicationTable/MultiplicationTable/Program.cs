c0eefd56f0e84052729fc2a629084d27c8026f99
arminamirinasab <arminamirinasab@gmail.com>
2022-12-19 21:12:25 +0330

    Add MultiplicationTable Excercise C#

---
+++ Excercises/C#/MultiplicationTable/MultiplicationTable/Program.cs
@@ -0,0 +1,14 @@
+﻿// Higher Order Loop
+for (int z = 1; z <= 10; z++)
+{
+    // Loop Per Row
+    for (int i = 1; i <= 10; i++)
+    {
+        // Print Numbers
+        Console.Write("\t{0}", i * z);
+    }
+    // Horzintal Line
+    Console.WriteLine("\n -----------------------------------------------------------------------------------------");
+}
+
+Console.ReadKey();
