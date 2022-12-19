e886fdc7ff6035650cd5ad28521ab6563d11f2ff
arminamirinasab <arminamirinasab@gmail.com>
2022-12-19 17:40:08 +0330

    Add ReturnScore Excercise C#

---
+++ Excercises/C#/ReturnScore/ReturnScore/Program.cs
@@ -0,0 +1,16 @@
+﻿// Variables
+int count = 20;
+double sum = 0;
+// Loop
+for (int i = 1; i <= count; i++)
+{
+    // Get Value
+    Console.Write("Enter Score {0}: " , i);
+    int userScore = int.Parse(Console.ReadLine());
+    // Sum Numbers
+    sum += userScore;
+}
+// Calculate Average & Show Result
+Console.WriteLine("\n Average Of Scroes: {0}" , sum / count);
+
+Console.ReadKey();
