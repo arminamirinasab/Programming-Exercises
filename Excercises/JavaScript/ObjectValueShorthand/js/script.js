4543e65a439912ce72001f4a53ad52eab8e2d1e7
arminamirinasab <arminamirinasab@gmail.com>
2022-12-19 14:46:36 +0330

    Add ObjectValueShorthand Excercise JS

---
+++ Excercises/JavaScript/ObjectValueShorthand/js/script.js
@@ -0,0 +1,17 @@
+// Variables
+let name = "Armin";
+let lastname = "Amiri Nasab";
+let age = 18;
+let job = "FrontEnd Developer";
+let favoriteLang = "JavaScript";
+// Use Object Value Shorthand
+let userObject = {
+    name,
+    lastname,
+    age,
+    job,
+    favoriteLang
+}
+// Result
+alert("Open Code & See Result In The Console");
+console.log(userObject);
