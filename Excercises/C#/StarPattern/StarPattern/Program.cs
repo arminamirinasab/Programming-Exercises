b074795140a7c389aae5f8430fc5f28125f47dcd
arminamirinasab <arminamirinasab@gmail.com>
2022-12-19 20:59:06 +0330

    Add StarPattern Excercise C#

---
+++ Excercises/C#/StarPattern/StarPattern/Program.cs
@@ -0,0 +1,27 @@
+﻿// Variable
+int count = 5;
+// Star Loop
+for (int i = 0; i < count; i++)
+{
+    // Internal Loop
+	for (int x = 0; x <= i; x++)
+	{
+        Console.Write("*");
+    }
+    // Space
+    Console.WriteLine("");
+}
+// Numbers Loop
+for (int i = 0; i < count; i++)
+{
+    // Internal Loop
+    for (int x = 0; x <= i; x++)
+    {
+        // Add 1 For Loop Parity
+        Console.Write(x + 1);
+    }
+    // Space
+    Console.WriteLine("");
+}
+
+Console.ReadKey();
