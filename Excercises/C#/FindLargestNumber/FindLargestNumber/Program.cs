f8d57c4721f0e3035c3e919b1714d459a2ca9c8b
arminamirinasab <arminamirinasab@gmail.com>
2022-12-19 19:23:08 +0330

    Add FindLargestNumber Excercise C#

---
+++ Excercises/C#/FindLargestNumber/FindLargestNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿// Loop Variable
+int i = 0;
+// Largest Number
+int lastNumber = 0;
+// Help
+Console.WriteLine("Enter 0 To Show Result :)");
+while (true)
+{
+    i++;
+    // Get Value
+    Console.Write("Enter Number {0}: ", i);
+    int userNumber = int.Parse(Console.ReadLine());
+    // Break For 0
+    if (userNumber == 0)
+    {
+        break;
+    }
+    // Check & Replace Larger Number
+    if(userNumber > lastNumber)
+    {
+        lastNumber = userNumber;
+    }
+}
+// Show Result
+Console.WriteLine("\n Largest Number: {0}", lastNumber);
+
+Console.ReadKey();
