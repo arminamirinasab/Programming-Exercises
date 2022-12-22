839e84b118db3b9884813ceae9b5ae8d7393665c
arminamirinasab <arminamirinasab@gmail.com>
2022-12-22 09:47:06 +0330

    Add ReverseArrayWithFor Excercise C#

---
+++ Excercises/C#/ReverseArrayWithFor/foreach/Program.cs
@@ -0,0 +1,17 @@
+﻿// Array
+string[] students = { "Armin", "Sedna", "Mahdi", "Abolfazl", "Fateme", "Mona" };
+// New Empty Array To Assign
+string[] newStudents = new string[students.Length];
+// Reverse Array With For
+for (int i = students.Length - 1, sort = 0; i >= 0; i--)
+{
+    newStudents[sort] = students[i];
+    sort++;
+}
+// Show Students
+foreach (var student in newStudents)
+{
+    Console.WriteLine(student);
+}
+
+Console.ReadKey();
