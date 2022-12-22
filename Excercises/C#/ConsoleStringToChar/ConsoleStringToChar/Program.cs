1282ced9bf2744b88e068c8fc29bbb8811318111
arminamirinasab <arminamirinasab@gmail.com>
2022-12-22 09:11:58 +0330

    Add ConsoleStringToChar Excercise C#

---
+++ Excercises/C#/ConsoleStringToChar/ConsoleStringToChar/Program.cs
@@ -0,0 +1,12 @@
+﻿// Get String
+Console.Write("Enter Your String: ");
+string userString = Console.ReadLine();
+// Loop
+for (int i = 0; i < userString.Length; i++)
+{
+    // Convert To Char & Print
+    char c = userString[i];
+    Console.WriteLine(c);
+}
+
+Console.ReadKey();
