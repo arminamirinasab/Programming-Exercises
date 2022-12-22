0433d606c60361caf2f1c804b2c2ba30238ef8d5
arminamirinasab <arminamirinasab@gmail.com>
2022-12-22 13:36:01 +0330

    Add ConsoleFindInArray Excercise C#

---
+++ Excercises/C#/ConsoleFindInArray/Poodman2/Program.cs
@@ -0,0 +1,33 @@
+﻿// Arrays
+string[] students = new string[50];
+double[] scores = new double[50];
+// Get Student Loop
+for (int i = 0;i < students.Length; i++)
+{
+    // Get Value
+    Console.Write("Please Enter Studnet Name: ");
+    string student = Console.ReadLine();
+    Console.Write("Please Enter Studnet Score: ");
+    int score = int.Parse(Console.ReadLine());
+    Console.WriteLine();
+    // Check 0 & Break
+    if(score == 0 || student == "0")
+    {
+        break;
+    }
+    // Assign Values
+    students[i] = student;
+    scores[i] = score;
+}
+
+// Show Students & Filter
+for (int i = 0; i < students.Length; i++)
+{
+    // Condition
+    if (scores[i] <= 10 && scores[i] != 0)
+    {
+        Console.Write("Student: {0} ; Score: {1} \n", students[i], scores[i]);
+    }
+}
+
+Console.ReadKey();
