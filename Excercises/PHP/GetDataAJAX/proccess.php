3f8f9fc537e5b3527d2f86e9bac41fcd22646571
arminamirinasab <arminamirinasab@gmail.com>
2023-01-06 13:04:08 +0330

    Add GetDataAJAX Excercise PHP

---
+++ Excercises/PHP/GetDataAJAX/proccess.php
@@ -0,0 +1,4 @@
+<?php
+$countries = ["Iran" , "Turkey" , "USA" , "Ireland" , "Ireland" , "Netherland" , "Poland" , "Ukrine" , "Yemen" , "Brazil" , "Kenya" , "Iraq" , "UAE" , "Afghanistan" , "England" , "France" , "Germany" , "Russia" , "India" , "Pakistan"];
+// Send Response
+echo $countries[array_rand($countries)];
